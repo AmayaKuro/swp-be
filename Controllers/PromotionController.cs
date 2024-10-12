40d8e827b36835ad819a85e7be39a84806dcd711
NguyenAnh <anhnse171217@fpt.edu.vn>
2024-10-13 01:52:56 +0700

    CRUD Promotion

---
+++ Controllers/PromotionController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using swp_be.Data;
+using swp_be.Models;
+using swp_be.Services;
+
+namespace swp_be.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PromotionController : ControllerBase
+    {
+        private readonly PromotionService promotionService;
+
+        public PromotionController(ApplicationDBContext context)
+        {
+            promotionService = new PromotionService(context);
+        }
+
+        // GET: api/Promotion
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Promotion>>> GetPromotions()
+        {
+            return Ok(await promotionService.GetPromotions());
+        }
+
+        // GET: api/Promotion/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Promotion>> GetPromotion(int id)
+        {
+            var promotion = await promotionService.GetPromotions();
+            var foundPromotion = promotion.FirstOrDefault(p => p.PromotionID == id);
+
+            if (foundPromotion == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(foundPromotion);
+        }
+
+        // POST: api/Promotion
+        [HttpPost]
+        public async Task<ActionResult<Promotion>> CreatePromotion(Promotion promotion)
+        {
+            await promotionService.CreatePromotion(promotion);
+            return CreatedAtAction(nameof(GetPromotion), new { id = promotion.PromotionID }, promotion);
+        }
+
+        // PUT: api/Promotion/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePromotion(int id, Promotion promotion)
+        {
+            if (id != promotion.PromotionID)
+            {
+                return BadRequest();
+            }
+
+            await promotionService.UpdatePromotion(promotion);
+
+            return NoContent();
+        }
+
+        // DELETE: api/Promotion/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePromotion(int id)
+        {
+            var promotions = await promotionService.GetPromotions();
+            var promotion = promotions.FirstOrDefault(p => p.PromotionID == id);
+            if (promotion == null)
+            {
+                return NotFound();
+            }
+
+            await promotionService.DeletePromotion(promotion);
+            return NoContent();
+        }
+
+        // GET: api/Promotion/Search
+        [HttpGet("Search")]
+        public async Task<ActionResult<IEnumerable<Promotion>>> SearchPromotions([FromQuery] string? code, [FromQuery] DateTime? startDate, [FromQuery] DateTime? endDate)
+        {
+            return Ok(await promotionService.SearchPromotions(code, startDate, endDate));
+        }
+    }
+}
